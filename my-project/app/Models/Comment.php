83121a69f978fdf7321a7f98ce887af6aec20983
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-24 10:01:53 +0200

    comment init

---
+++ my-project/app/Models/Comment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'content',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($post) {
+            $post->user_id = Auth::id();
+        });
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
