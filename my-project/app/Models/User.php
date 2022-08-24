e4e6a14929b898034ee4d5eebdc495fc718fd8b9
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-24 06:23:54 +0200

    post relationship added

--- my-project/app/Models/User.php
+++ my-project/app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(UsersMetaData::class);
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
