de7255cc6c8992813d8592fe4685a38456679128
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-24 13:13:07 +0200

    policies for soft-delete added

---
+++ my-project/app/Policies/PostPolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore()
+    {
+        return Auth::user()->is_admin;
+    }
+
+    /**
+     * Determine whether the user can force delete the model.
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete()
+    {
+        return Auth::user()->is_admin;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Post  $model
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Post $model)
+    {
+        return $user->is_admin || (auth()->check() && $model->user_id == auth()->id());
+    }
+}
