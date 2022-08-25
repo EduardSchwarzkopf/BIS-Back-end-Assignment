2651c1252c8179aa0feb7dfee355cb9e2ebdcf01
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-25 13:26:23 +0200

    Login tests added

--- my-project/tests/Feature/AuthTest.php
+++ my-project/tests/Feature/AuthTest.php
@@ -2,21 +2,35 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_UserLoginSuccessfull()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
 
-        $response->assertStatus(200);
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => "password"
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_UserLoginFailed()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => "wrongPassword"
+        ]);
+
+        $response->assertStatus(401);
     }
 }
