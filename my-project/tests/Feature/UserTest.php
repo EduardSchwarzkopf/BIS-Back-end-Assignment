b607cb85f026ae4f2b5105a9d05f6782171599ee
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-25 22:29:59 +0200

    added is_admin tests

--- my-project/tests/Feature/UserTest.php
+++ my-project/tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,55 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    private $adminKey = 'is_admin';
+
+    public function test_SetIsAdmin()
+    {
+        $token = UserUtility::adminAccessToken();
+
+        $payload[$this->adminKey] = true;
+
+        $user =  User::factory()->create();
+
+        $response = UserUtility::authApiRequest($this, '/users/' . $user->id, $token, 'PUT', $payload);
+
+        $response->assertOk();
+
+        $responseData = $response->json()['data'];
+
+        $this->assertEquals($payload[$this->adminKey], $responseData[$this->adminKey]);
+    }
+
+    public function test_SetIsAdminFail()
+    {
+        $user = UserUtility::user();
+        $token = UserUtility::accessToken($user);
+
+        $payload[$this->adminKey] = true;
+
+        $response = UserUtility::authApiRequest($this, '/users/' . $user->id, $token, 'PUT', $payload);
+
+        $response->assertUnprocessable();
+    }
+
+    public function test_SetIsAdminWrongType()
+    {
+        $user = UserUtility::admin();
+        $token = UserUtility::accessToken($user);
+
+        $typeList = [
+            'string',
+            123,
+            3.14,
+            [1, 2, 3]
+        ];
+
+        foreach ($typeList as $type) {
+            $payload[$this->adminKey] = $type;
+
+            $response = UserUtility::authApiRequest($this, '/users/' . $user->id, $token, 'PUT', $payload);
+            $response->assertUnprocessable();
+        }
     }
 }
