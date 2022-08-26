083e91ed29c934bfb9af80e28e1cd2bd71521b2c
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-26 06:45:34 +0200

    update password test added

--- my-project/tests/Feature/AuthTest.php
+++ my-project/tests/Feature/AuthTest.php
@@ -33,4 +33,30 @@ class AuthTest extends TestCase
 
         $loginResponse->assertCreated();
     }
+
+    public function test_UpdatePasswordAndLogin()
+    {
+        $user = UserUtility::user();
+        $token = UserUtility::accessToken($user);
+
+        $newPassword = 'NewPassword';
+        $reponse = UserUtility::authApiRequest($this, '/users/' . $user->id, $token, 'PUT', [
+            'password' => $newPassword
+        ]);
+
+        $reponse->assertOk();
+
+        $reponse = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => $newPassword
+        ]);
+
+        $reponse->assertCreated();
+        $reponse = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $reponse->assertUnauthorized();
+    }
 }
