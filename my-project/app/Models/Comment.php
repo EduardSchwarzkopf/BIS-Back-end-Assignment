1746813d87ce841cb88ae1c6d9e1d03375744c66
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-08-27 16:00:39 +0200

    added pruning

--- my-project/app/Models/Comment.php
+++ my-project/app/Models/Comment.php
@@ -4,17 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
 
 class Comment extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Prunable;
 
     protected $fillable = [
         'post_id',
         'content',
     ];
+
+    public function prunable()
+    {
+        return static::where('created_at', '<=', now()->subHours(3));
+    }
 
     protected static function booted()
     {
